4f9f802665e4354d44eece8c89c04d4835397426
changan-memories <3324148542@qq.com>
2024-07-21 15:19:19 +0800

    update

---
+++ 5_2二叉链表/5_2二叉链表/main.cpp
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+//经常找后继的   用二叉链表
+typedef struct BiNode {
+	int data;	//	数据域
+	struct BiNode *LChild, *RChild;	//指针域   指向左右孩子
+}BiNode, *BiTree;	//前面是结点   后面是指向该结点的指针
+
+int main() {
+
+
+
+
+	return 0;
+}
