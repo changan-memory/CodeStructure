b26399833d5c9b9d7fd3fd556da8a531e4a07be8
changan-memories <3324148542@qq.com>
2024-07-23 15:15:08 +0800

    update

--- 5_2二叉链表/5_2二叉链表/main.cpp
+++ 5_2二叉链表/5_2二叉链表/main.cpp
@@ -62,20 +62,23 @@ bool InitBinTree(BiTree T, int data) {
 	T->Rchild = NULL;
 	return true;
 }
-
+// 利用栈 实现 中序非递归遍历
 bool InOrderTraverse_N(BiTree T) {
 	BiTree p = T;
 	BiTree q = NULL;
 	InitStack(S);
-	while (p || !EmptyStack(S)) {
-		if (p) {
+	// p 指向结点  从根结点开始
+	while (p || !EmptyStack(S)) {	
+		if (p) {	// 如果 结点不为空  则该结点入栈 同时 p 指向该结点的左子树 
 			Push(S, q);
 			p = p->Lchild;
 		}
-		else {
-			Pop(S, q);
+		else {	// 如果 结点为空  说明 左 已经访问完, 此时的栈顶元素为 子树的根 
+			Pop(S, q);	// 栈顶元素出栈，并用q 指向出栈元素，即 q 指向子树的根
+
 			printf("%d ", 1);//实际是 q->data   这里是因为 二叉树还为建立  就打印了 1
 			assert(q);
+			// 访问完 p 之后 ， p 指向 总根 的右子树，进行 下一轮 判断
 			p = q->Rchild;
 		}
 	}
