de580d9b31376c6d31b3777b4bcbb5be1fcceddb
changan-memories <3324148542@qq.com>
2024-07-28 09:47:57 +0800

    update

--- 5_10顺序存储构造哈夫曼树/5_10顺序存储构造哈夫曼树/main.cpp
+++ 5_10顺序存储构造哈夫曼树/5_10顺序存储构造哈夫曼树/main.cpp
@@ -8,8 +8,18 @@ typedef struct HTNode {
 	int parent, LChlid, RChild;	//表示双亲和左右孩子  在数组中的下标
 }HTNode, *HuffmanTree;
 
-void Select(HuffmanTree HT, int& s1, int& s2) {
-
+void Select(HuffmanTree HT, int i, int& s1, int& s2) {	// 要遍历 1 到 i (需要包含新建立的结点)
+	//int min_1 = 0, min_2 = 0;
+	s1 = HT[1].weight;	//存储 最小 和 第二小 的权重
+	s2 = HT[1].weight;
+	for (int j = 1; j <= i; j++) {
+		if (HT[j].weight <= s1 && HT[j].parent == 0)
+			s1 = HT[j].weight;
+	}
+	for (int j = 1; j <= i; j++) {
+		if (HT[j].weight <= s2 && HT[j].weight >= s1 && HT[j].parent == 0)
+			s2 = HT[j].weight;
+	}
 }
 // 构造一个结构数组 存放 结点, 数组中的第一个空间不放结点  n个叶子结点，共有 (2n-1) 个结点
 // 数组大小定为 2n  刚好可以放下  1--(2n-1)
@@ -30,9 +40,8 @@ void creatHuffmanTree(HuffmanTree HT, int n) {
 		std:: cin >> HT[i].weight;
 	// 初始化过后 生成哈夫曼树
 	int s1 = 0, s2 = 0;
-	//Select(HT, s1, s2);
 	for (int i = n + 1; i <= m; i++) {
-		//Select(HT, s1, s2);
+		Select(HT, i-1, s1, s2);
 		HT[s1].parent = i;
 		HT[s2].parent = i;
 		HT[i].LChlid = s1;
